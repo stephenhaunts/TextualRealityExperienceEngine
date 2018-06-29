c9dfd748b228e6d3aad3c234ac42a2462f2ebe65
stephen haunts <stephen.haunts@outlook.com>
2018-06-29 10:12:36 +0100

    Add unit test projects.

---
+++ TextualRealityExperienceEngine/Tests.Unit.GameEngine/Tests.Unit.Compiler/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TextualRealityExperienceEngine.GameEngine.Tests.Unit.Compiler
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
