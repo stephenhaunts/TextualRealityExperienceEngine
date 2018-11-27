19dd4123c7719f79593faf5f14c321f14eb89845
stephen haunts <stephen.haunts@outlook.com>
2018-11-27 13:38:32 +0000

    Add enumeration for directions.

---
+++ TextualRealityExperienceEngine/GameEngine/Directions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TextualRealityExperienceEngine.GameEngine
+{
+    public enum Directions
+    {
+        North = 0,
+        East = 1,
+        South = 2,
+        West = 3,
+        NorthEast = 4,
+        SouthEast = 5,
+        SouthWest = 6,
+        NorthWest = 7
+    }
+}
