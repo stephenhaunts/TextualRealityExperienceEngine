d40d8aafa35daa9fc23dbbb982dd2c810fa2b20f
stephen haunts <stephen.haunts@outlook.com>
2019-04-16 10:34:21 +0100

    Refactor game setup for the simple game to make testing easier.

---
+++ TextualRealityExperienceEngine/Tests.SimpleGame.Library/SimpleGameController.cs
@@ -0,0 +1,167 @@
+﻿/*
+MIT License
+
+Copyright (c) 2019 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Tests.SimpleGame.Crypt;
+using Tests.SimpleGame.Downstairs;
+using Tests.SimpleGame.UpStairs;
+using TextualRealityExperienceEngine.GameEngine;
+using TextualRealityExperienceEngine.GameEngine.Interfaces;
+
+namespace Tests.SimpleGame.Library
+{
+    public class SimpleGameController
+    {
+        private IGame Game = new Game();
+        private IRoom Outside;
+        private IRoom Hallway;
+        private IRoom Lounge;
+        private IRoom Garage;
+        private IRoom Kitchen;
+        private IRoom DiningRoom;
+        private IRoom UpstairsHallway;
+
+        public string Prologue
+        {
+            get
+            {
+                return Game.Prologue;
+            }
+        }
+
+        public string CurrentRoomDescription
+        {
+            get
+            {
+                return Game.CurrentRoom.Description;
+            }
+        }
+
+        public void InitializeGame()
+        {
+            Game = new Game();
+
+            AddContentItems();
+
+            Game.Prologue = Game.ContentManagement.RetrieveContentItem("Prologue");
+            Game.HelpText = Game.ContentManagement.RetrieveContentItem("HelpText");
+            Game.HintSystemEnabled = true;
+
+            SetupRooms();
+
+            Game.StartRoom = Outside;
+            Game.CurrentRoom = Outside;
+        }
+
+        public void SetupRooms()
+        {
+            Outside = new Outside(Game);
+            Garage = new Garage(Game);
+            Kitchen = new Kitchen(Game);
+            DiningRoom = new DiningRoom(Game);
+            UpstairsHallway = new UpstairsHallway(Game);
+
+            Lounge = new Lounge(Game);
+            Hallway = new Hallway(Game)
+            {
+                LightsOn = false
+            };
+
+            var doorway = new DoorWay
+            {
+                Direction = Direction.North,
+                Locked = true,
+            };
+
+            var doorwayToGarage = new DoorWay
+            {
+                Direction = Direction.NorthWest,
+                Locked = true,
+            };
+
+            Outside.AddExit(doorway, Hallway);
+            Outside.AddExit(doorwayToGarage, Garage);
+            Hallway.AddExit(Direction.West, Lounge);
+            Hallway.AddExit(Direction.North, Kitchen);
+            Hallway.AddExit(Direction.NorthEast, UpstairsHallway);
+
+            Kitchen.AddExit(Direction.East, DiningRoom);
+        }
+
+        public void AddContentItems()
+        {
+            Game.ContentManagement.AddContentItem("NoNeedToBeRude", "There is no need to be rude.");
+            Game.ContentManagement.AddContentItem("Prologue", "Welcome to the test adventure from the Textual Reality Experience Engine. \r\n\r\nYou will be bedazzled with amazement at the sheer awesomeness of our graphics engine.");
+
+            Game.ContentManagement.AddContentItem("HelpText", "Your aim is to find the treasure that is hidden somewhere in the house. \r\nYou need to type commands into the game to control the player.");
+
+            Game.ContentManagement.AddContentItem("AreYouSure", "Are you sure? (y/n) : ");
+            Game.ContentManagement.AddContentItem("ExitMessage", "Have it your way.. You spontaneously combust and depart this mortal coil in a puff of smoke....");
+        }
+
+        public GameReply ProcessCommand(string command)
+        {
+            return Game.ProcessCommand(command);
+        }
+
+        public string RetrieveContentItem(string item)
+        {
+            return Game.ContentManagement.RetrieveContentItem(item);
+        }
+
+        public int NumberOfMoves
+        {
+            get
+            {
+                return Game.NumberOfMoves;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return Game.Score;
+            }
+        }
+
+        public ReadOnlyCollection<string> GetInventory()
+        {
+            return Game.Player.Inventory.GetInventory();
+        }
+
+        public List<(string name, string description)> GetVisitedRooms()
+        {
+            return Game.VisitedRooms.GetVisitedRooms();
+        }
+
+        public string Help
+        {
+            get
+            {
+                return Game.HelpText;
+            }
+        }
+    }
+}
--- TextualRealityExperienceEngine/Tests.SimpleGame/Program.cs
+++ TextualRealityExperienceEngine/Tests.SimpleGame/Program.cs
@@ -22,102 +22,29 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 using System;
-using Tests.SimpleGame.Downstairs;
-using Tests.SimpleGame.Crypt;
+using Tests.SimpleGame.Library;
 using TextualRealityExperienceEngine.GameEngine;
-using TextualRealityExperienceEngine.GameEngine.Interfaces;
-using Tests.SimpleGame.UpStairs;
 
 namespace Tests.SimpleGame
 {
-    internal class Program
-    {
-        private static IGame _game = new Game(); 
-        private static IRoom _outside;
-        private static IRoom _hallway;
-        private static IRoom _lounge;
-        private static IRoom _garage;
-        private static IRoom _kitchen;
-        private static IRoom _diningRoom;
-        private static IRoom _upstairsHallway;
-
-        private static void InitializeGame()
+    public class Program
+    {    
+        public static void Main()
         {
-            _game = new Game();
-
-            AddContentItems();
-
-            _game.Prologue = _game.ContentManagement.RetrieveContentItem("Prologue");
-            _game.HelpText = _game.ContentManagement.RetrieveContentItem("HelpText");
-            _game.HintSystemEnabled = true;
-
-            SetupRooms();
-
-            _game.StartRoom = _outside;
-            _game.CurrentRoom = _outside;
-        }
-
-        private static void SetupRooms()
-        {
-            _outside = new Outside(_game);
-            _garage = new Garage(_game);
-            _kitchen = new Kitchen(_game);
-            _diningRoom = new DiningRoom(_game);
-            _upstairsHallway = new UpstairsHallway(_game);
-
-            _lounge = new Lounge(_game);
-            _hallway = new Hallway(_game)
-            {
-                LightsOn = false
-            };
-
-            var doorway = new DoorWay
-            {
-                Direction = Direction.North,
-                Locked = true,
-            };
-
-            var doorwayToGarage = new DoorWay
-            {
-                Direction = Direction.NorthWest,
-                Locked = true,
-            };
-
-            _outside.AddExit(doorway, _hallway);
-            _outside.AddExit(doorwayToGarage, _garage);
-            _hallway.AddExit(Direction.West, _lounge);
-            _hallway.AddExit(Direction.North, _kitchen);
-            _hallway.AddExit(Direction.NorthEast, _upstairsHallway);
-
-            _kitchen.AddExit(Direction.East, _diningRoom);
-        }
-
-        private static void AddContentItems()
-        {
-            _game.ContentManagement.AddContentItem("NoNeedToBeRude", "There is no need to be rude.");
-            _game.ContentManagement.AddContentItem("Prologue", "Welcome to the test adventure from the Textual Reality Experience Engine. \r\n\r\nYou will be bedazzled with amazement at the sheer awesomeness of our graphics engine.");
-                          
-            _game.ContentManagement.AddContentItem("HelpText", "Your aim is to find the treasure that is hidden somewhere in the house. \r\nYou need to type commands into the game to control the player.");
-
-            _game.ContentManagement.AddContentItem("AreYouSure", "Are you sure? (y/n) : ");
-            _game.ContentManagement.AddContentItem("ExitMessage", "Have it your way.. You spontaneously combust and depart this mortal coil in a puff of smoke....");           
-        }
-
-        private static void Main()
-        {
+            SimpleGameController simpleGame = new SimpleGameController();
             Console.Clear();
-            InitializeGame();
+            simpleGame.InitializeGame();
 
-            ConsoleEx.WordWrap(_game.Prologue);
+            ConsoleEx.WordWrap(simpleGame.Prologue);
             Console.WriteLine();
 
-            ConsoleEx.WordWrap(_game.StartRoom.Description);
+            ConsoleEx.WordWrap(simpleGame.CurrentRoomDescription);
             Console.WriteLine();
 
             while (true)
             {
                 Console.Write("> ");
-                var reply = _game.ProcessCommand(Console.ReadLine());
+                var reply = simpleGame.ProcessCommand(Console.ReadLine());
 
                 switch (reply.State)
                 {
@@ -135,13 +62,13 @@ namespace Tests.SimpleGame
 
                     case GameStateEnum.Exit:
                         Console.WriteLine();
-                        Console.Write(_game.ContentManagement.RetrieveContentItem("AreYouSure"));
+                        Console.Write(simpleGame.RetrieveContentItem("AreYouSure"));
                         var response = Console.ReadLine();
 
                         if (response != null && response.ToLower() == "y")
                         {
                             Console.WriteLine();
-                            ConsoleEx.WordWrap(_game.ContentManagement.RetrieveContentItem("ExitMessage"));
+                            ConsoleEx.WordWrap(simpleGame.RetrieveContentItem("ExitMessage"));
 
                             Environment.Exit(0);
                         }
@@ -149,13 +76,13 @@ namespace Tests.SimpleGame
 
                     case GameStateEnum.Score:
                         Console.WriteLine();
-                        Console.WriteLine("You have made " + _game.NumberOfMoves + " so far.");
-                        Console.WriteLine("You score is " + _game.Score);
+                        Console.WriteLine("You have made " + simpleGame.NumberOfMoves + " so far.");
+                        Console.WriteLine("You score is " + simpleGame.Score);
                         Console.WriteLine();
                         continue;
 
                     case GameStateEnum.Inventory:
-                        if (_game.Player.Inventory.Count() == 0)
+                        if (simpleGame.GetInventory().Count == 0)
                         {
                             Console.WriteLine();
                             Console.WriteLine("Your inventory is empty.");
@@ -167,7 +94,7 @@ namespace Tests.SimpleGame
                             Console.WriteLine("Inventory");
                             Console.WriteLine("---------");
 
-                            foreach (var i in _game.Player.Inventory.GetInventory())
+                            foreach (var i in simpleGame.GetInventory())
                             {
                                 Console.WriteLine("   " + i);
                             }
@@ -183,7 +110,7 @@ namespace Tests.SimpleGame
                         Console.WriteLine("You have visited the following locations.");
                         Console.WriteLine();
 
-                        foreach (var location in _game.VisitedRooms.GetVisitedRooms())
+                        foreach (var location in simpleGame.GetVisitedRooms())
                         {
                             Console.WriteLine("    " + location.name);
                         }
@@ -196,7 +123,7 @@ namespace Tests.SimpleGame
                         Console.WriteLine("Game Manual");
                         Console.WriteLine("-----------");
                         Console.WriteLine();
-                        ConsoleEx.WordWrap(_game.HelpText);
+                        ConsoleEx.WordWrap(simpleGame.Help);
                         continue;
                 }
             }
