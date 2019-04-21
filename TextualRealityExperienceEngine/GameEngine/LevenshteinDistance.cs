f739624f718f015e0f0fb4c995a119ad429295b5
Stephen Haunts <stephen.haunts@outlook.com>
2019-04-21 22:17:33 +0100

    Add code for performing a Levenshtein Distance comparison of 2 strings.

---
+++ TextualRealityExperienceEngine/GameEngine/LevenshteinDistance.cs
@@ -0,0 +1,80 @@
+﻿/*
+MIT License
+
+Copyright(c) 2019 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
+using TextualRealityExperienceEngine.GameEngine.Interfaces;
+
+namespace TextualRealityExperienceEngine.GameEngine
+{
+    public class LevenshteinDistance : IStringDistance
+    {
+        public int Distance(string source, string target)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (string.IsNullOrEmpty(target))
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
+
+            source = source.ToLower();
+            target = target.ToLower();
+
+            var distance = new int[source.Length + 1, target.Length + 1];
+
+            for (var i = 0; i <= source.Length; i++)
+            {
+                distance[i, 0] = i;
+            }
+
+            for (var j = 0; j <= target.Length; j++)
+            {
+                distance[0, j] = j;
+            }
+
+            for (var j = 1; j <= target.Length; j++)
+            {
+                for (var i = 1; i <= source.Length; i++)
+                {
+                    if (source[i - 1] == target[j - 1])
+                    {
+                        distance[i, j] = distance[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        distance[i, j] = Math.Min(Math.Min(
+                        distance[i - 1, j] + 1, //a deletion
+                        distance[i, j - 1] + 1), //an insertion
+                        distance[i - 1, j - 1] + 1 //a substitution
+                        );
+                    }
+                }
+            }
+
+            return distance[source.Length, target.Length];
+        }
+    }
+}
---
+++ TextualRealityExperienceEngine/Tests.Unit.GameEngine/LevenshteinDistanceTests.cs
@@ -0,0 +1,103 @@
+﻿/*
+MIT License
+
+Copyright(c) 2019 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextualRealityExperienceEngine.GameEngine;
+using TextualRealityExperienceEngine.GameEngine.Interfaces;
+
+namespace TextualRealityExperienceEngine.Tests.Unit.GameEngine
+{
+    [TestClass]
+    public class LevenshteinDistanceTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowArgumentNullExceptionIfSourceIsNull()
+        {
+            IStringDistance distance = new LevenshteinDistance();
+            distance.Distance(null, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowArgumentNullExceptionIfTargetIsNull()
+        {
+            IStringDistance distance = new LevenshteinDistance();
+            distance.Distance("name", null);
+        }
+
+        [TestMethod]
+        public void CalculateDistanceOfKittenAndSitten()
+        {
+            IStringDistance distance = new LevenshteinDistance();
+            Assert.AreEqual(3, distance.Distance("kitten", "sitting"));
+        }
+
+        [TestMethod]
+        public void CalculateDistanceOfKittenAndSittenWithDifferentCases()
+        {
+            IStringDistance distance = new LevenshteinDistance();
+            Assert.AreEqual(3, distance.Distance("Kitten", "siTTing"));
+        }
+
+        [TestMethod]
+        public void CalculateDistanceOfGetAndGrtWithDifferentCases()
+        {
+            IStringDistance distance = new LevenshteinDistance();
+            Assert.AreEqual(1, distance.Distance("Get", "Grt"));
+        }
+
+        [TestMethod]
+        public void CalculateDistanceWithSourceLongerThanTarget()
+        {
+            IStringDistance distance = new LevenshteinDistance();
+            int actual = distance.Distance("Stephen", "Steve");
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void CalculateDistanceWithSourceShorterThanTarget()
+        {
+            IStringDistance distance = new LevenshteinDistance();
+            int actual = distance.Distance("Steve", "Stephen");
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void CalculateDistanceWithSourceShorterThanTargetAndTotallyUnrelated()
+        {
+            IStringDistance distance = new LevenshteinDistance();
+            int actual = distance.Distance("Mat", "Geoffery");
+            Assert.AreEqual(8, actual);
+        }
+
+        [TestMethod]
+        public void CalculateDistanceWithSingleCharacters()
+        {
+            IStringDistance distance = new LevenshteinDistance();
+            int actual = distance.Distance("a", "b");
+            Assert.AreEqual(1, actual);
+        }
+    }
+}
