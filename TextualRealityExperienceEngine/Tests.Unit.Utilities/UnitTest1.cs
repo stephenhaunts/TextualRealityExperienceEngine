bb0eda656e1dccf71e7c052a1eebdbeba07ecccd
Stephen Haunts <stephen.haunts@outlook.com>
2020-06-01 10:59:14 +0100

    Add unit testing library for the utilities code.

---
+++ TextualRealityExperienceEngine/Tests.Unit.Utilities/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TextualRealityExperienceEngine.Tests.Unit.Utilities
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
